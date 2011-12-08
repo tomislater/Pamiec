03134f24de931d418710ecfc6b4b100c9aefb69a
Tomasz Święcicki <tomislater@gmail.com>
2011-12-09 00:33:16 +0100

    Całkowicie nowa wersja. Zapraszam.

--- pamiec.py
+++ pamiec.py
@@ -1,92 +1,117 @@
-#!/usr/bin/env python -tt
 # -*- coding: utf-8 -*-
 '''
 @author: tomislater@gmail.com
 '''
-import codecs, os, random, getopt
-__version__ = '0.2.8'
+import codecs, os, random, sys
+__version__ = '0.3'
 
-class Pamiec(object):
-  def __init__(self, argv): 
-    try:
-      opts, args = getopt.getopt(argv, 'pl:d:w', ['pomoc', 'losuj=', 'dodaj=', 'wersja'])
-    except getopt.GetoptError:
-      self.pomoc()
-      sys.exit()
+class Slownik(object):
+  u"""Tworzy słownik w którym przechowywane są słowa."""
+  def __init__(self):
+    u"""Wywołuje ładowanie słownik do 'słownika pythonowego'"""
+    self.zaladuj_slownik() 
 
-    for opt, arg in opts:
-      if opt in ('-p', '--pomoc'):
-        self.pomoc()
-      elif opt in ('-d', '--dodaj'):
-        self.zaladuj_slownik()
-        self.dodaj(arg)
-      elif opt in ('-l', '--losuj'):
-        try:
-          self.zaladuj_slownik()
-          self.losuj(int(arg))
-        except ValueError:
-          print '\nMusisz podać liczbę!\n'
-      elif opt in ('--wersja', '-w'):
-        print 'Wersja programu:'.rjust(23), __version__, '\n'
-  
-  
+
   def zaladuj_slownik(self):
+    u"""Ładuje słownik do 'słownika pythonowego'"""
     try:
       self.slownik = {}
       for slowo in codecs.open(os.path.abspath(__file__)[:os.path.abspath(__file__).rfind('/')+1] + 'slowa.txt', encoding='utf-8'):
         self.slownik[slowo[:-1]] = ''
     except IOError:
       print u'Prawdopodobnie nie robisz tego tak jak trzeba. Uruchom program z lini poleceń.'
-      
-  
-  def zapisz_slownik(self):
-    slownik = codecs.open(os.path.abspath(__file__)[:os.path.abspath(__file__).rfind('/')+1] + 'slowa.txt', 'w', encoding='utf-8')
-    for slowo in self.slownik:
-      slownik.write('%s\n' % slowo)
-    slownik.close()
-      
-  
-  def losuj(self, liczba_slow):
-    import copy
-    slownikKopia = copy.copy(self.slownik) 
-    try:
-      print '\n|', '-'*23, '|'
-      for liczbaSlowa in xrange(liczba_slow):
-        slowo = random.choice(slownikKopia.keys())
-        print '|'.ljust(1), str(liczbaSlowa+1).ljust(3), slowo
-        del slownikKopia[slowo]
-      print '|', '-'*23, '|\n'
-    except IndexError:
-      print '|', '-'*23, '|'
-      print '\nSprawdź, plik. Najprawdopodobniej, nie ma w nim tyle słów.\nWyświetliłem ile dało radę.\n'
 
-  
+
   def dodaj(self, slowa):
+    u"""Dodaje słowa przechwycone z lini poleceń do 'słonika pythonowego'"""
     slowa = slowa.split(',')
     for slowo in slowa:
       if slowo != '' and len(slowo) > 1:
         self.slownik[codecs.decode(slowo, 'utf-8')] = ''
     self.zapisz_slownik()
-  
-  
-  def pomoc(self):
-    print u"""\nDostępne polecenia:
-    -l lub --losuj
-              losuje podaną liczbę słów
-              -l 30 ----- losuje 30 liczb
-              --losuj 20 ----- losuje 20 liczb
-    -d lub --dodaj
-              dodaje podane słowa
-              -d 'mistyfikacja,dupa,biskup' ----- dodaje podane słowa
-              --dodaj 'płyta,ptaszek,bazylia' ----- również dodaje podane słowa
-                        Uwaga! Pamiętaj, aby rozdzielać słowa przecinkami.
-    -p lub --pomoc
-              pomoc :)
-    -w lub --wersja
-              wyświetla numer aktualnej wersji\n"""
-              
-              
 
+  
+  def zapisz_slownik(self):
+    u"""Zapisuje 'pythonowy słownik' do pliku"""
+    slownik = codecs.open(os.path.abspath(__file__)[:os.path.abspath(__file__).rfind('/')+1] + 'slowa.txt', 'w', encoding='utf-8')
+    for slowo in self.slownik:
+      slownik.write('%s\n' % slowo)
+
+
+  def _or(self, liczba_slow):
+    u"""Odpowiada za wyświetlanie metod:
+    - zakładki obrazkowe
+    - rymowanki liczbowe"""
+    import copy
+    slownikKopia = copy.copy(self.slownik) 
+    try:
+      print '\n|', '-'*19, '|'
+      for liczbaSlowa in xrange(liczba_slow):
+        slowo = random.choice(slownikKopia.keys())
+        print '|'.ljust(1), str(liczbaSlowa+1).ljust(3), slowo
+        del slownikKopia[slowo]
+      print '|', '-'*19, '|\n'
+    except IndexError:
+      print '|', '-'*19, '|'
+      print '\nSprawdź, plik. Najprawdopodobniej, nie ma w nim tyle słów.\nWyświetliłem tyle ile było w pliku.\n'
+  
+  
+  def lms(self, liczba_slow):
+    u"""Odpowiada za wyświetlenie metody "Łańcuhowa metoda skojarzeń"""
+    import copy
+    slownikKopia = copy.copy(self.slownik) 
+    try:
+      print '\n|', '-'*19, '|'
+      for liczba in xrange(liczba_slow):
+        slowo = random.choice(slownikKopia.keys())
+        print '|'.ljust(1), slowo
+        del slownikKopia[slowo]
+      print '|', '-'*19, '|\n'
+    except IndexError:
+      print '|', '-'*19, '|'
+      print '\nSprawdź, plik. Najprawdopodobniej, nie ma w nim tyle słów.\nWyświetliłem ile dało radę.\n'
+
+
+def pomoc():
+  u"""Wyświetla pomoc."""
+  print u"""Dostępne polecenia:
+  python pamiec.py DODAJ 'slowo1,slowo2,slowo3' - dodaje podane słowa
+  python pamiec.py LMS <liczba> - wyświetla określoną liczbę słów (Łańcuchowa metoda skojarzeń)
+  python pamiec.py OR <liczba> - wyświetla określoną liczbę słów" (Metody: Zakładki obrazkowe oraz rymowanki liczbowe)
+  python pamiec.py POMOC - wyświetla pomoc
+  python pamiec.py WERSJA - wyświetla aktualną wersję programu
+  
+  Przykłady:
+  python pamiec.py DODAJ 'dupa,pleb,lumpenproletariat'
+  python pamiec.py LMS 10
+  python pamiec.py OR 20\n"""
+  
+  
+  
 if __name__ == '__main__':
-  import sys
-  Pamiec(sys.argv[1:])
+  if len(sys.argv) == 3:
+    if sys.argv[1] == 'DODAJ':
+      Slownik().dodaj(sys.argv[2])
+    elif sys.argv[1] == 'OR':
+      try:
+        Slownik()._or(int(sys.argv[2]))
+      except ValueError:
+        pomoc()
+        print 'Argument musi być liczbą.\n'
+    elif sys.argv[1] == 'LMS':
+      try:
+        Slownik().lms(int(sys.argv[2]))
+      except ValueError:
+        pomoc()
+        print 'Argument musi być liczbą.\n'
+    else:
+      pomoc()
+  elif len(sys.argv) == 2:
+    if sys.argv[1] == 'POMOC':
+      pomoc()
+    elif sys.argv[1] == 'WERSJA':
+      print 'Wersja programu:', __version__, '\n'
+    else:
+      pomoc()
+  else:
+    pomoc()
