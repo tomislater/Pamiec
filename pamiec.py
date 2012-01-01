bb782c57bde92bceeb5eed9e99fee669d9d9ea0c
Tomasz Święcicki <tomislater@gmail.com>
2012-01-01 23:46:18 +0100

    Dodałem nową funkcjonalność.

--- pamiec.py
+++ pamiec.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-'''
-@author: tomislater@gmail.com
-'''
+
+__author__ =  'tomislater@gmail.com'
+
 import codecs, os, random, sys
-__version__ = '0.3'
+__version__ = '0.4'
 
 class Slownik(object):
   u"""Tworzy słownik w którym przechowywane są słowa."""
@@ -72,24 +72,79 @@ class Slownik(object):
       print '\nSprawdź, plik. Najprawdopodobniej, nie ma w nim tyle słów.\nWyświetliłem ile dało radę.\n'
 
 
+
+class ListaLiczb(object):
+  _listaLiczb = []
+  def __init__(self, ilosc, dlugosc):
+    zakres = self.zwrocZakres(dlugosc)
+    self.ilosc = ilosc
+    self.stworzRandomy(zakres)
+    self.wyswietl(dlugosc)
+
+  def zwrocZakres(self, dlugosc):
+    if dlugosc == 3:
+      return (100, 999)
+    elif dlugosc == 4:
+      return (1000, 9999)
+    elif dlugosc == 5:
+      return (10000, 99999)
+    elif dlugosc == 6:
+      return (100000, 999999)
+    elif dlugosc == 7:
+      return (1000000, 9999999)
+    elif dlugosc == 8:
+      return (10000000, 99999999)
+    elif dlugosc == 9:
+      return (100000000, 999999999)
+    else:
+      print >>sys.stderr, 'Liczba długości musi mieścić się w przedziale <3;9>\n'
+      pomoc()
+      sys.exit(2)
+
+  def stworzRandomy(self, zakres):
+    while self.ilosc:
+      rLiczba = random.randint(zakres[0], zakres[1])
+      if rLiczba not in self._listaLiczb:
+        self._listaLiczb.append(rLiczba)
+      self.ilosc -= 1
+    return
+
+  def wyswietl(self, dlugosc):
+    for liczba in self._listaLiczb:
+      print "\n    {0}".format(liczba)
+    print
+
+
+
 def pomoc():
   u"""Wyświetla pomoc."""
   print u"""Dostępne polecenia:
   python pamiec.py DODAJ 'slowo1,slowo2,slowo3' - dodaje podane słowa
   python pamiec.py LMS <liczba> - wyświetla określoną liczbę słów (Łańcuchowa metoda skojarzeń)
   python pamiec.py OR <liczba> - wyświetla określoną liczbę słów" (Metody: Zakładki obrazkowe oraz rymowanki liczbowe)
+  python pamiec.py LICZBY <liczbaIlosc> <liczbaDlugosc> - wyswietla randomowe liczby o zadanej dlugosci
   python pamiec.py POMOC - wyświetla pomoc
   python pamiec.py WERSJA - wyświetla aktualną wersję programu
   
   Przykłady:
   python pamiec.py DODAJ 'dupa,pleb,lumpenproletariat'
   python pamiec.py LMS 10
-  python pamiec.py OR 20\n"""
+  python pamiec.py OR 20
+  python pamiec.py LICZBY 10 7\n"""
   
   
   
 if __name__ == '__main__':
-  if len(sys.argv) == 3:
+  if len(sys.argv) == 4:
+    try:
+      ilosc = int(sys.argv[2])
+      dlugosc = int(sys.argv[3])
+    except ValueError:
+      print >>sys.stderr, 'Argumentami muszą być liczby\n'
+      pomoc()
+      sys.exit(2)
+    ListaLiczb(ilosc, dlugosc)
+  elif len(sys.argv) == 3:
     if sys.argv[1] == 'DODAJ':
       Slownik().dodaj(sys.argv[2])
     elif sys.argv[1] == 'OR':
